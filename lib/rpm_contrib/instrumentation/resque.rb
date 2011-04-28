16e640db89961ed8af37b6a72d41d0c74b031934
James Adam <james@lazyatom.com>
2011-04-28 12:26:00 +0100

    Added a bunch of debugging to see if NewRelic is messing with our workers.

--- lib/rpm_contrib/instrumentation/resque.rb
+++ lib/rpm_contrib/instrumentation/resque.rb
@@ -13,20 +13,32 @@ module RPMContrib
         old_perform_method = instance_method(:perform)
 
         define_method(:perform) do
+          NewRelic::Agent.instance.log.info "Starting NewRelic injected perform"
           class_name = (payload_class ||self.class).name
+          NewRelic::Agent.instance.log.info "reseting stats"
           NewRelic::Agent.reset_stats if NewRelic::Agent.respond_to? :reset_stats
+          NewRelic::Agent.instance.log.info "performing action with newrelic trace"
           perform_action_with_newrelic_trace(:name => 'perform', :class_name => class_name,
                                              :category => 'OtherTransaction/ResqueJob') do
-            old_perform_method.bind(self).call
+            NewRelic::Agent.instance.log.info "calling original perform method"
+            r = old_perform_method.bind(self).call
+            NewRelic::Agent.instance.log.info "returned from original perform with #{r.inspect}"
+            r
           end
-
-          NewRelic::Agent.shutdown unless defined?(::Resque.before_child_exit)
+          unless defined?(::Resque.before_child_exit)
+            NewRelic::Agent.instance.log.info "calling agent shutdown"
+            NewRelic::Agent.shutdown
+            NewRelic::Agent.instance.log.info "done calling agent shutdown"
+          end
+          NewRelic::Agent.instance.log.info "finally leaving injected perform"
         end
       end
 
       if defined?(::Resque.before_child_exit)
         ::Resque.before_child_exit do |worker|
+          NewRelic::Agent.instance.log.info "calling agent shutdown in before_child_exit"
           NewRelic::Agent.shutdown
+          NewRelic::Agent.instance.log.info "done calling agent shutdown"
         end
       end
     end
